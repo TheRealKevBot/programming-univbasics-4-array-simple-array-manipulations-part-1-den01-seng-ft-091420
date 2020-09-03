f7c0c9280d5b5e2ad20180e4646cef471097e95b
Kevin Glasgow <kglas001@gmail.com>
2020-09-03 18:29:57 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,11 +3,17 @@ def using_push(array, string)
   array.push(string)
 end 
 
-def using_unshift 
+def using_unshift(array, string)
+  array.unshift(string)
 end 
 
-def pop_with_args 
+def pop(array, string)
+  array.pop(string)
 end 
+
+def pop_with_args(array, string)
+  array.
+end
 
 def using_shift 
 end 
